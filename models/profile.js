bc59b3862e550985eccb49e9bee842a6e2ff9322
WalterGuerra <wsteven00@gmail.com>
2023-09-23 13:16:09 -0400

    add schemas for favorite team and player in profile model

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,27 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const favoritePlayerSchema = new Schema({
+  name: String,
+  sport: String, 
+  team: String, 
+  age: Number
+}, {
+  timestamps: true
+})
+
+const favoriteTeamSchema = new Schema({
+  name: String, 
+  sport: String, 
+  achievments: String, 
+  topPlayers: String,
+})
+
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  favoriteplayer: [favoritePlayerSchema],
+  favortieteam: [favoriteTeamSchema]
 }, {
   timestamps: true
 })
