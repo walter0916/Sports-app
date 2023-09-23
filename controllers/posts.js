c890fa5a676bf1de5b4f771bd20a241d4c237113
WalterGuerra <wsteven00@gmail.com>
2023-09-23 14:54:57 -0400

    add form to add post on index view and add function in index view to print all posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -3,7 +3,10 @@ import { Post } from "../models/post.js"
 function index(req, res) {
   Post.find({})
   .then(posts => {
-    res.render('posts/index')
+    res.render('posts/index', {
+      posts, 
+      title: 'all posts'
+    })
   })
   .catch(err => {
     console.log(err)
