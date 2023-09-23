7cc565312200e5f4d1b6ac395e4d32bf0caf2091
WalterGuerra <wsteven00@gmail.com>
2023-09-23 16:35:01 -0400

    add a tag in index view to go to a posts show view and add show function to render that view

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,7 +27,23 @@ function create(req, res) {
   })
 }
 
+function show(req, res) {
+  Post.findById(req.params.postId)
+  .populate('author')
+  .then(post => {
+    res.render('posts/show', {
+      post,
+      title: 'All replies'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/posts')
+  })
+}
+
 export {
   index,
-  create
+  create,
+  show
 }
