c1a076963ed3cc9eec1d475bef066cc9a215bbd3
WalterGuerra <wsteven00@gmail.com>
2023-09-23 13:06:56 -0400

    add routes for profiles and posts

---
+++ routes/posts.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { isLoggedIn } from '../middleware/middleware.js'
+
+const router = Router()
+
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Home Page' })
+})
+
+export {
+  router
+}
